99ff65ec56bf99c35ff1a97f03a2053eba441060
filelink503-cmd <filelink503@gmail.com>
2026-03-13 12:08:52 +0530

    fix loop issue & remove plugins load

--- bot.py
+++ bot.py
@@ -2,14 +2,11 @@
 # Subscribe YouTube Channel For Amazing Bot @BabuBhaiKundan
 # Ask Doubt on telegram @kundan_yadav_bot
 
-import sys
-import glob
-import importlib
 import logging
 import logging.config
 import pytz
 import asyncio
-from pathlib import Path
+
 from datetime import date, datetime
 from aiohttp import web
 
@@ -32,13 +29,9 @@ from BabuBhaiKundan.bot import BabuBhaiKundanBot
 from BabuBhaiKundan.util.keepalive import ping_server
 from BabuBhaiKundan.bot.clients import initialize_clients
 
-# Plugin files
-ppath = "plugins/*.py"
-files = glob.glob(ppath)
-
 
 async def start():
-    
+
     print("\nStarting Bot...\n")
 
     # Start bot
@@ -49,22 +42,6 @@ async def start():
 
     # Initialize assistant clients
     await initialize_clients()
-
-    # Load plugins
-    for name in files:
-        with open(name):
-            patt = Path(name)
-            plugin_name = patt.stem
-
-            plugins_dir = Path(f"plugins/{plugin_name}.py")
-            import_path = f"plugins.{plugin_name}"
-
-            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-            load = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(load)
-
-            sys.modules[import_path] = load
-            print("Imported Plugin =>", plugin_name)
 
     # Render / Heroku keepalive
     if ON_HEROKU:
@@ -89,20 +66,22 @@ async def start():
         text=script.RESTART_TXT.format(today, time_now)
     )
 
-    # Web server
+    # Start web server
     app = web.AppRunner(await web_server())
     await app.setup()
 
     bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, int(PORT)).start()
+    await web.TCPSite(app, bind_address, PORT).start()
 
-    print("Web Server Started")
+    print(f"🌐 Web Server Started on Port {PORT}")
 
     await idle()
 
 
 if __name__ == "__main__":
+
     try:
         asyncio.run(start())
+
     except KeyboardInterrupt:
         logging.info("Service Stopped Bye 👋")
